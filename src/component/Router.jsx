3eac6ff742b43eabb68ce63697924e4d4d1de2e5
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-04-07 09:25:59 -0400

    haciendo el componente para soportar rutas dinamicas

--- src/component/Router.jsx
+++ src/component/Router.jsx
@@ -1,10 +1,21 @@
 import { useLink } from '../customHook/useLink'
-
+import { Children } from 'react'
+import { match } from 'path-to-regexp'
 export function Router({ routes = [], defaul: DefaultScreen = () => null }) {
   const { path } = useLink()
 
+  const routeparametro = {}
+
   const Scrn = routes.find(value => {
-    return value.path == path
+    if (value.path == path) return true
+
+    const dinamic = match(value.path, { decode: decodeURIComponent })
+    const matched = dinamic(path)
+
+    if (!matched) return false
+
+    routeparametro = matched.params
+    return true
   })?.Component
 
   return Scrn ? <Scrn /> : <DefaultScreen />
